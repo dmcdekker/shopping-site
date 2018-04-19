f60614b18134cb1b29aa6ddec4a64493193605fb
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-19 13:02:59 -0700

    finish add_to_cart

--- shoppingsite.py
+++ shoppingsite.py
@@ -8,6 +8,7 @@ Authors: Joel Burton, Christian Fernandez, Meggie Mahnken, Katie Byers.
 
 from flask import Flask, render_template, redirect, flash, session
 import jinja2
+from flask_debugtoolbar import DebugToolbarExtension
 
 import melons
 
@@ -47,7 +48,7 @@ def show_melon(melon_id):
     Show all info about a melon. Also, provide a button to buy that melon.
     """
 
-    melon = melons.get_by_id("meli")
+    melon = melons.get_by_id(melon_id)
     return render_template("melon_details.html",
                            display_melon=melon)
 
@@ -95,26 +96,27 @@ def add_to_cart(melon_id):
     #   dictionary keyed to the string "cart")
     #if cart not in session:
     melon = melons.get_by_id(melon_id)
-    print melon.common_name
 
-    # if session['cart']:
-    #     cart = session['cart']
-    #     cart[melon_id] = (common_name.get('quantity', 0)) + 1 
 
-    # #   if not
-    # else:
-    #     session['cart'] = {}
-    #     cart = session['cart']
-    #     cart[melon_id] = (melon_id.common_name.get('quantity', 0)) + 1 
 
-    print session    
+    
+    if session.get('cart') is None:
+        session['cart'] = {}    
+    else:
+        cart = session['cart']
+        cart[melon_id] = cart.get(melon_id, 0) + 1 
+
+    print session
+
+    
+
         
     # - check if the desired melon id is the cart, and if not, put it in
     # - increment the count for that melon id by 1
     # - flash a success message
     # - redirect the user to the cart page
-
-    return "Oops! This needs to be implemented!"
+    flash("Congrats!")
+    return render_template("cart.html")
 
 
 @app.route("/login", methods=["GET"])
@@ -161,4 +163,6 @@ def checkout():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.debug = True
+    DebugToolbarExtension(app)
+    app.run(host="0.0.0.0")
